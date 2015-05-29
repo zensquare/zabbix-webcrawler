3e90166fa610f50fd0e42929b666a57f3bfd1367
x1alpha76 <alpha@zensquare.com>
2015-05-29 11:56:30 +0930

    Moved from the dashboard-extension repo
    Untested since move - added schema file

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # zabbix-webcrawler
 A webcrawler designed to be used with zabbix to check the health of a website
+
+## Work in progress
+It's early days still but functional in my setup but treat as experimental.
+
+## Getting started
+- Download
+- Setup the database ( mysql < schema.sql)
+- mv/cp config.php.example config.php
+- Test with:
+  - php crawl.php "your url" scan //Returns how many pages were scanned(run multiple times)
+  - php crawl.php "your url" status.links //Will tell you how many urls have been found
+  - php crawl.php "your url" status.links //Will tell you home many links have been found
+  - php crawl.php "your url" status.deadurls //Will tell you how many urls have returned a status other than 200
+  - php crawl.php "your url" status.deadlinks //Will tell you how many links have return a status other than 200
+
+## Configure in zabbix
+- Setup zabbix agent - add the following to zabbix_agent.conf or in a new zabbix_agent.d/.conf file.
+ - UserParameter=spider[*],/usr/bin/php <PATH TO CRAWL.php>/crawl.php $1 $2
+-Import the webcrawler template into zabbix (template.xml)
+-Add template to hosts - the template expects the host name to be a domain name. In the future this may change to use a URL macro.
+
+##Setup options
+You can install this on any* server that has the zabbix_agent installed - just configure the agent interface to point to this server
+
+##Config options
+TBD
---
+++ schema.sql
@@ -0,0 +1,109 @@
+CREATE DATABASE  IF NOT EXISTS `zabbix_spider` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `zabbix_spider`;
+-- MySQL dump 10.13  Distrib 5.6.17, for Win32 (x86)
+--
+-- Host: localhost    Database: zabbix_spider
+-- ------------------------------------------------------
+-- Server version	5.1.73
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `access`
+--
+
+DROP TABLE IF EXISTS `access`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `access` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `page` int(11) DEFAULT NULL,
+  `ts` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `size` int(11) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
+  `response` varchar(45) DEFAULT NULL,
+  `link_count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`aid`),
+  KEY `fk_page_idx` (`page`),
+  CONSTRAINT `fk_page` FOREIGN KEY (`page`) REFERENCES `page` (`aid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1619141 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `link`
+--
+
+DROP TABLE IF EXISTS `link`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `link` (
+  `parent` int(11) NOT NULL,
+  `child` int(11) NOT NULL,
+  `link_title` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`parent`,`child`),
+  KEY `fk_page_idx` (`child`),
+  CONSTRAINT `fk_link_parent` FOREIGN KEY (`parent`) REFERENCES `page` (`aid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `page`
+--
+
+DROP TABLE IF EXISTS `page`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `page` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `website` int(11) DEFAULT NULL,
+  `title` varchar(256) DEFAULT NULL,
+  `url` varchar(1024) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `ts_first` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `ts_last` timestamp NULL DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `crawl` int(1) DEFAULT '1',
+  PRIMARY KEY (`aid`),
+  KEY `fk_website_idx` (`website`),
+  CONSTRAINT `fk_website` FOREIGN KEY (`website`) REFERENCES `website` (`aid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=1396 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `website`
+--
+
+DROP TABLE IF EXISTS `website`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `website` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `url` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`aid`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'zabbix_spider'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-05-29 11:24:59
